ae08a44b5d340535ce5cce8ecfb4ae876956f616
Deanna Warren <mrs.deanna.warren@gmail.com>
2016-01-20 21:18:08 -0800

    Add user CRUD routes and views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,21 @@
 class UserController < ApplicationController
 
-  def index
+  def create
+    user = User.new(user_params)
+    user.title = 'user'
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(id: user.id)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+  end
+
+  def edit
+    @user = User.find(session[:user_id])
   end
 
   def show
@@ -11,5 +26,26 @@ class UserController < ApplicationController
     @products = Product.all
     render 'show'
   end
+
+  def update
+    user = User.find(session[:user_id])
+    if user.update(user_params)
+      redirect_to user_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    user = User.find(session[:user_id])
+    user.delete
+    redirect_to root_path
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :password)
+    end
 
 end
