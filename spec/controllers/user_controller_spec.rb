02c4fbd8f8fa072c0358af8882fa0b4a999b5598
Mia Lehrer <mia.lehrer@gmail.com>
2016-01-21 11:25:58 -0800

    fix error in factory

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,31 +1,75 @@
 require 'spec_helper'
 
+require 'rails_helper'
+RSpec.describe UserController, type: :controller do
 
+  describe UserController do
+    let(:admin) { FactoryGirl.create :admin }
+    let(:attributes_admin) { FactoryGirl.attributes_for :admin}
+    let(:user) { FactoryGirl.create :user}
+    let(:attributes_user) { FactoryGirl.attributes_for :user}
 
-RSpec.describe UserController, :type => :controller do
-
-  describe "User is logged out" do
-    it 'cannot access user page' do
-      visit '/user/1'
-      expect(page).to_not have_selector(".logout")
+    describe "GET #index" do
+      before(:each) do
+        session[:user_id] = user.id
+        get :index
+      end
+      it "responds with a 200" do
+        expect(response.status).to eq(200)
+      end
+      it "assigns the users instance variable" do
+        expect(assigns(:users)).to be_a(ActiveRecord::Relation)
+      end
     end
 
-    it 'allows login' do
-      visit '/login'
-      fill_in 'Username', :with => 'admin'
-      fill_in 'Password', :with => 'password'
-      click_button 'Log in'
-      expect(page).to have_selector(".homepage-link-container")
-    end
-  end
+    describe 'POST #create' do
+      it "saves a new user in the database" do
+        expect{
+          post :create, :user => attributes}.to change{ User.count}.by(1)
+        end
 
-  describe 'User is logged in' do
-    it 'allows logout' do
-      visit '/login'
-      fill_in 'Username', :with => 'admin'
-      fill_in 'Password', :with => 'password'
-      click_button 'Log in'
-    expect(page).to have_selector(".logout")
+        it "redirects to the root path" do
+          post :create, :user => attributes
+          expect(response.status).to eq(302)
+        end
+      end
+
+      # describe 'GET #show' do
+      #   before(:each) do
+      #     get :show, id: user.idbe rspec
+      #   end
+
+      #   it "assigns the user instance variable" do
+      #     expect(assigns[:user]).to be_a(User)
+      #   end
+      # end
+
+      describe 'PUT #update' do
+        let!(:username) { "UPDATED_USERNAME" }
+        it 'updates a field on an user' do
+          put :update, id: user.id, user: { username: username}
+          expect(user.reload.username).to eq(user.username)
+        end
+      end
+
+      describe 'DELETE #destroy' do
+      before(:each) do
+        session[:user_id] = admin.id
+      end
+        it 'decrements users by one' do
+          user_id = user.id
+          expect {
+            delete :destroy, id: user_id
+            }.to change{ User.count }.by(-1)
+          end
+          it 'redirects to the root path' do
+            user_id = user.id
+            delete :destroy, id: user_id
+            expect(response.status).to eq(302)
+          end
+        end
+
+      end
     end
-  end
-end
+
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,7 @@ require File.expand_path("../../config/environment", __FILE__)
 RSpec.configure do |config|
 
   config.include Capybara::DSL
+  config.include FactoryGirl::Syntax::Methods
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
