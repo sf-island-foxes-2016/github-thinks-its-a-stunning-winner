483d6940159a4880b1eafec339710bd6dcc59c89
Jon Clayton <Jonclayton@swbell.net>
2016-01-23 15:15:18 -0800

    Heroku deploy bug hunt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,10 +49,10 @@ function addToCart(user_id, product_id){
   var cartId = user_id;
   //post to cart#create(product id, quantity if !=1)
   $.ajax({
-  //   type: "PUT",
-  //   url: "/carts/" + cartId,
-  //  data: {product_id},
-  //  })
+    type: "PUT",
+    url: "/carts/" + cartId,
+   data: {product_id},
+   })
   // .done(function(response){
   //   $('#cartstuffiguess').html(response)
   })
